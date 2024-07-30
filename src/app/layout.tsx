10860b56aa3430cf20c5a4f9947d3374cde0d2c1
Yelyzaveta Demchenko <demcenkoelizaveta6@gmail.com>
2024-07-30 20:59:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="xzDi9bz9GT4c_oET0O9Ubv4nsAoDPZL9bgYYYT3blXw"
+        />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="A Star Wars app built with Next.js that shows the list of starwars characters and their individual characteristics"
+        />
+        <title>Starwars | Discover starwars characters</title>
+        <meta
+          name="keywords"
+          content="starwars, starwars characters, starwars films, starwars species, starwars starships"
+        />
+        <meta name="author" content="Starwars App" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://star-wars-app-lyart.vercel.app/" />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
